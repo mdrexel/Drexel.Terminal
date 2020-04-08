646358b5c378e8da727b1e07e58b4b84d3f35036
Max Drexel <mdrexel@osisoft.com>
2020-04-08 15:55:31 -0400

    Port text (no idea if it works yet)

--- Drexel.Terminal.Layout/Layouts/Symbols/Label.cs
+++ Drexel.Terminal.Layout/Layouts/Symbols/Label.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Drexel.Terminal.Internals;
 using Drexel.Terminal.Sink;
 using Drexel.Terminal.Text;
@@ -7,17 +10,25 @@ namespace Drexel.Terminal.Layout.Layouts.Symbols
 {
     public class Label : Symbol
     {
+        private static readonly short NewLineLength = (short)Environment.NewLine.Length;
+        private static readonly string[] NewLine = new string[] { Environment.NewLine };
+
         private readonly Catena content;
         private readonly Alignments alignments;
+        private readonly TerminalColors? backgroundFill;
 
         private readonly object cacheLock;
         private CharInfo[,] cached;
+        private IReadOnlyList<TextLine> lines;
+
+        private ushort preceedingLinesSkipped;
 
         public Label(
             IResizeableRegion region,
             string name,
             Catena content,
-            Alignments alignments)
+            Alignments alignments,
+            TerminalColors? backgroundFill = null)
             : base(region, name)
         {
             if (content is null)
@@ -27,6 +38,8 @@ namespace Drexel.Terminal.Layout.Layouts.Symbols
 
             this.content = content;
             this.alignments = alignments;
+            this.backgroundFill = backgroundFill;
+            this.preceedingLinesSkipped = 0;
 
             this.cacheLock = new object();
             this.cached = null!;
@@ -38,28 +51,189 @@ namespace Drexel.Terminal.Layout.Layouts.Symbols
                         {
                             lock (this.cacheLock)
                             {
-                                this.cached = null!;
+                                if (x.ChangeTypes.HasFlag(RegionChangeTypes.Resize))
+                                {
+                                    this.MaximumVisibleLines = (ushort)x.AfterChange.ActualHeight;
+                                    this.cached =
+                                        this.Generate(x.BeforeChange.ActualWidth != x.AfterChange.ActualWidth);
+                                }
+
                                 this.RequestRedraw(x.BeforeChange, x.AfterChange);
                             }
                         })));
+
+            this.cached = this.Generate(true);
         }
 
         public override bool CanBeFocused => false;
 
         public override bool CapturesTabKey => false;
 
-        public override void Draw(ITerminalSink sink, Rectangle window)
-        {
-            if (this.cached is null)
-            {
-            }
+        public ushort TotalLines { get; private set; }
 
-            Coord destination = new Coord(window.Left, window.Top);
-            sink.Write(this.cached!, destination, window - destination);
+        public ushort PreceedingLinesSkipped
+        {
+            get => this.preceedingLinesSkipped;
+            private set
+            {
+                lock (this.cacheLock)
+                {
+                    this.preceedingLinesSkipped = (ushort)Math.Min(value, this.TotalLines - this.MaximumVisibleLines);
+                    this.cached = this.Generate(false);
+                    this.RequestRedraw();
+                }
+            }
         }
 
-        private CharInfo[,] Generate()
+        public ushort MaximumVisibleLines { get; private set; }
+
+        public override void Draw(ITerminalSink sink, Rectangle window)
         {
+            lock (this.cacheLock)
+            {
+                Coord destination = new Coord(window.Left, window.Top);
+                sink.Write(this.cached!, destination, window - destination);
+            }
+        }
+
+
+        public void AdjustLinesSkipped(int delta)
+        {
+            if (delta == 0)
+            {
+                return;
+            }
+
+            lock (this.cacheLock)
+            {
+                checked
+                {
+                    if (delta < 0)
+                    {
+                        delta = -delta;
+                        if (delta > this.PreceedingLinesSkipped)
+                        {
+                            this.PreceedingLinesSkipped = 0;
+                        }
+                        else
+                        {
+                            this.PreceedingLinesSkipped -= (ushort)delta;
+                        }
+                    }
+                    else
+                    {
+                        this.PreceedingLinesSkipped += (ushort)delta;
+                    }
+                }
+
+                this.cached = this.Generate(false);
+                this.RequestRedraw();
+            }
+        }
+
+        [DebuggerDisplay("{StartOffset,nq}: {Content} ({Content.Length,nq})")]
+        private class TextLine
+        {
+            public TextLine(string content, ushort startOffset)
+            {
+                this.Content = content;
+                this.StartOffset = startOffset;
+            }
+
+            public string Content { get; }
+
+            public ushort StartOffset { get; }
+        }
+
+        private CharInfo[,] Generate(bool widthChanged)
+        {
+            this.MaximumVisibleLines = (ushort)this.Region.ActualHeight;
+
+            if (widthChanged)
+            {
+                string[] rawLines = this.content.Value.Split(NewLine, StringSplitOptions.None);
+                TextLine[] buffer = new TextLine[rawLines.Length];
+
+                TextLine previous = new TextLine(string.Empty, (ushort)-NewLineLength);
+                for (int counter = 0; counter < rawLines.Length; counter++)
+                {
+                    string raw = rawLines[counter];
+                    TextLine line = new TextLine(raw, (ushort)(previous.StartOffset + previous.Content.Length + NewLineLength));
+                    buffer[counter] = line;
+                    previous = line;
+                }
+
+                List<TextLine> calculated = new List<TextLine>();
+                short width = this.Region.ActualWidth;
+                foreach (TextLine line in buffer)
+                {
+                    if (line.Content.Length <= width)
+                    {
+                        calculated.Add(line);
+                    }
+                    else
+                    {
+                        // Line is too wide to fit the current region - need to split it into multiple lines.
+                        string[] words = line.Content.Split(' ');
+                        ushort sumOfLengths = 0;
+                        ushort startOffset = line.StartOffset;
+                        int lastLineEndedOnIndex = 0;
+                        for (int counter = 0; counter < words.Length; counter++)
+                        {
+                            string word = words[counter];
+                            if (word.Length > width)
+                            {
+                                // Word is longer than an entire line. We need to:
+                                // 1. Stop the current line and add it to the output list
+                                // 2. Split the word into as many lines as would be *ENTIRELY FILLED* by it and add them to the list
+                                // 3. Resume on the last partially-filled line
+                                throw new NotImplementedException("Word too long");
+                            }
+                            else
+                            {
+                                sumOfLengths = (ushort)(sumOfLengths + word.Length);
+                                bool writeLine = sumOfLengths == width || counter == words.Length - 1;
+                                if (sumOfLengths > width)
+                                {
+                                    // Line is too long - write the line excluding the current word
+                                    sumOfLengths = (ushort)(sumOfLengths - word.Length);
+                                    counter--;
+                                    writeLine = true;
+                                }
+                                else
+                                {
+                                    // Line isn't filled yet - keep going
+                                    sumOfLengths++;
+                                }
+
+                                if (writeLine)
+                                {
+                                    // Line is filled - write the current line, including the current word
+                                    TextLine newLine = new TextLine(
+                                        string.Join(
+                                            " ",
+                                            words,
+                                            Math.Max(0, lastLineEndedOnIndex),
+                                            counter - lastLineEndedOnIndex + 1),
+                                        startOffset);
+                                    calculated.Add(newLine);
+
+                                    startOffset += sumOfLengths;
+                                    sumOfLengths = 0;
+                                    lastLineEndedOnIndex = counter + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                this.lines = calculated;
+                this.TotalLines = (ushort)this.lines.Count;
+
+                // Intentionally set lines skipped to itself, so that overflow is evaluated
+                this.PreceedingLinesSkipped = this.PreceedingLinesSkipped;
+            }
+
             if (this.alignments.HorizontalAlignment == HorizontalAlignment.Left)
             {
                 if (this.alignments.VerticalAlignment == VerticalAlignment.Top)
@@ -111,7 +285,35 @@ namespace Drexel.Terminal.Layout.Layouts.Symbols
 
         private CharInfo[,] GenerateTopLeft()
         {
-            throw new NotImplementedException();
+            CharInfo[,] output = new CharInfo[this.Region.ActualHeight, this.Region.ActualWidth];
+            if (this.backgroundFill.HasValue)
+            {
+                for (int y = 0; y < this.Region.ActualHeight; y++)
+                {
+                    for (int x = 0; x < this.Region.ActualWidth; x++)
+                    {
+                        output[y, x] = new CharInfo(' ', this.backgroundFill.Value);
+                    }
+                }
+            }
+
+            IEnumerator<TextLine> line = lines.Skip(this.PreceedingLinesSkipped).GetEnumerator();
+            for (int y = 0; y < this.Region.ActualHeight && line.MoveNext(); y++)
+            {
+                Range range = this.content.Ranges.GetRangeByIndex(line.Current.StartOffset);
+                for (int x = 0; x < line.Current.Content.Length; x++)
+                {
+                    int index = x + line.Current.StartOffset;
+                    if (index == range.EndIndexExclusive)
+                    {
+                        range = this.content.Ranges.GetNextRange(range);
+                    }
+
+                    output[y, x] = new CharInfo(line.Current.Content[x], range.Colors, range.Delay);
+                }
+            }
+
+            return output;
         }
 
         private CharInfo[,] GenerateCenterLeft()
